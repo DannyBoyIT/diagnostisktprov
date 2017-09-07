b104745a8742ed6c25d95f92eee593f0a46f9f42
Daniel <d_iliovits@windowslive.com>
2017-09-07 10:24:51 +0200

    Prepopulated ProductCategories

--- DiagnostisktProvProjekt/Data/DbInitializer.cs
+++ DiagnostisktProvProjekt/Data/DbInitializer.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using DiagnostisktProvProjekt.Data;
+using DiagnostisktProvProjekt.Models;
+using System.Linq;
 
 namespace PizzaDeluxe.Data
 {
     public class DbInitializer
     {
-        public static void Initialize()
+        public static void Initialize(ApplicationDbContext context,)
         {
+            if (!context.ProductCategories.Any())
+            {
+                var tv = new ProductCategory() { Name = "TV" };
+                var dvd = new ProductCategory() { Name = "DVD" };
+                var vhs = new ProductCategory() { Name = "VHS" };
+            }
         }
     }
 }
