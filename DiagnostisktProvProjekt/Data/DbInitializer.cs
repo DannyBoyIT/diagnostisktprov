cbc8eceb8e849f70e6b75986509c6d39a8905dc6
Daniel <d_iliovits@windowslive.com>
2017-09-07 09:50:27 +0200

    Changed to in memory database

---
+++ DiagnostisktProvProjekt/Data/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PizzaDeluxe.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize()
+        {
+        }
+    }
+}
