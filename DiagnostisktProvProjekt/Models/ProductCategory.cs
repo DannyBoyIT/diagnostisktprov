21fa2fd36c45b59309a976462e67225d8ddea4d2
Daniel <d_iliovits@windowslive.com>
2017-09-07 10:00:57 +0200

    Created Product category

---
+++ DiagnostisktProvProjekt/Models/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace DiagnostisktProvProjekt.Models
+{
+    public class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
